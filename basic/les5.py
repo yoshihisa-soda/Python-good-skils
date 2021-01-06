a0dc325740c0ccae4216927f9dd89e8c356b43f9
yoshihisa-soda <soda.06141031@gmail.com>
2021-01-07 07:55:48 +0900

    about tuple

---
+++ basic/les5.py
@@ -0,0 +1,21 @@
+"""
+() : parenthesis (丸括弧)
+[] : bracket (角括弧)
+{} : brace (波括弧)
+【】 : Gothic parenthesis (隅付き括弧)
+〈〉 : Angle brackets (山括弧)
+《》 : Double Angle brackets (二重山括弧)
+"""
+
+seat = []
+min = 0
+max = 5
+
+min <= len(seat) < max
+
+seat.append('p')
+seat.append('p')
+seat.append('p')
+seat.append('p')
+
+seat.pop(0)
---
+++ basic/lse6.py
@@ -0,0 +1,24 @@
+# タプルのアンパッキング
+
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(num_tuple)
+x, y = 10, 20
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+1 = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+print(i, j)
+
+a, b = 100, 200
+print(a, b)
+a, b = b, a
+print(a, b)
